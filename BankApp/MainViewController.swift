68ad008e6bd864b5c5f0ab28628aacfa82755931
JJB <jjb@macmini01.local>
2023-02-07 11:57:11 -0700

    Initial Commit

---
+++ BankApp/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//
+// BankApp
+// 
+// ViewController.swift
+//
+// Created by Royal Blue Software
+// 
+
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
